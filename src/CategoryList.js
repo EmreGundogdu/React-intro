049900390359216821657d8ef467a1ef29641fe9
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-06-01 16:45:02 +0300

    State

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -2,16 +2,27 @@ import React, { Component } from "react";
 import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class CategoryList extends Component {
+  state = {
+    categories: [
+      { CategoryId: 1, CategoryName: "Arabalar" },
+      { CategoryId: 2, CategoryName: "Satılık Evler" },
+      { CategoryId: 3, CategoryName: "Kiralık Daireler" },
+      { CategoryId: 4, CategoryName: "Ev Eşyaları" },
+      { CategoryId: 5, CategoryName: "Bahçe Malzemeleri" },
+      { CategoryId: 6, CategoryName: "Veri Türleri" },
+    ],
+  };
+
   render() {
     return (
       <div>
         <h3>{this.props.info.title} Component</h3>
         <ListGroup>
-          <ListGroupItem>Cras justo odio</ListGroupItem>
-          <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-          <ListGroupItem>Morbi leo risus</ListGroupItem>
-          <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
-          <ListGroupItem>Vestibulum at eros</ListGroupItem>
+          {this.state.categories.map((category) => (
+            <ListGroupItem key={category.CategoryId}>
+              {category.CategoryName}
+            </ListGroupItem>
+          ))}
         </ListGroup>
       </div>
     );
